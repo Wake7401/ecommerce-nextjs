7b991289e194f20f55b91a8147688ca26a7ad6db
Wake7401 <91364482+Wake7401@users.noreply.github.com>
2024-03-16 15:22:38 +0700

    create upload image and update billboard

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,4 +1,5 @@
 import prismadb from "@/lib/prismadb";
+import { BillboardForm } from "./components/billboard-form";
 
 const BillboardPage = async ({
   params,
@@ -10,7 +11,13 @@ const BillboardPage = async ({
       id: params.billboardId,
     },
   });
-  return <div>Existing Billboard: {billboard?.label}</div>;
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillboardForm initialData={billboard}/>
+      </div>
+    </div>
+  );
 };
 
 export default BillboardPage;
